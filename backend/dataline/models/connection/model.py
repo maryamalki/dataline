f2229a58f30788a86fced1b160695d4906677d81
Rami <rami.awar.ra@gmail.com>
2024-07-11 23:56:33 +0200

    feat: Added excel support

--- backend/dataline/models/connection/model.py
+++ backend/dataline/models/connection/model.py
@@ -15,6 +15,7 @@ class ConnectionModel(DBModel, UUIDMixin, kw_only=True):
     database: Mapped[str] = mapped_column("database", String, nullable=False)
     name: Mapped[str | None] = mapped_column("name", String)
     dialect: Mapped[str | None] = mapped_column("dialect", String)
+    type: Mapped[str] = mapped_column("type", String, nullable=False)
     is_sample: Mapped[bool] = mapped_column("is_sample", Boolean, nullable=False, default=False, server_default="false")
 
     # Relationships
---
+++ backend/dataline/services/parser/__init__.py
@@ -0,0 +1,3 @@
+from dataline.services.parser.excel_parser import ExcelParserService
+
+__all__ = ["ExcelParserService"]
